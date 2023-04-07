e66514a4e744a02d10cdadaf3d09e298d5bda83f
illia <ulyanov.iv@gmail.com>
2023-04-07 14:34:50 +0200

    Add graceful shutdown for consumer application

--- kafka-consumer-demo/src/main/java/ConsumerApp.java
+++ kafka-consumer-demo/src/main/java/ConsumerApp.java
@@ -1,5 +1,6 @@
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.errors.WakeupException;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -7,14 +8,17 @@ import org.slf4j.LoggerFactory;
 import java.time.Duration;
 import java.util.Collections;
 import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
 
 public class ConsumerApp {
     private static final Logger LOGGER = LoggerFactory.getLogger(ConsumerApp.class);
     private static final String KAFKA_SERVER = "localhost:9092";
     private static final String TOPIC_NAME = "coding-demo-01";
     private static final String CONSUMER_GROUP = "consumer-app-01";
+    private final KafkaConsumer<String, String> consumer;
+    private final CountDownLatch sync = new CountDownLatch(1);
 
-    public static void main(String[] args) {
+    public ConsumerApp() {
         // 1. Create KafkaConsumer properties
         var properties = new Properties();
         properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_SERVER);
@@ -23,20 +27,57 @@ public class ConsumerApp {
         properties.put(ConsumerConfig.GROUP_ID_CONFIG, CONSUMER_GROUP);
 
         // 2. Create KafkaConsumer instance
-        var consumer = new KafkaConsumer<String, String>(properties);
+        consumer = new KafkaConsumer<>(properties);
+    }
 
+    public void start() {
         // 3. Subscribe to a topic
         consumer.subscribe(Collections.singleton(TOPIC_NAME));
 
         // 4. Polling for records and print them out
-        while (true) {
-            LOGGER.info("Polling...");
-            var records = consumer.poll(Duration.ofSeconds(1));
-            if (!records.isEmpty()) {
-                for (var record : records) {
-                    LOGGER.info(record.toString());
+        pollAndPrint();
+    }
+
+    private void pollAndPrint() {
+        try {
+            while (true) {
+                LOGGER.info("Polling...");
+                var records = consumer.poll(Duration.ofSeconds(1));
+                if (!records.isEmpty()) {
+                    for (var record : records) {
+                        LOGGER.info(record.toString());
+                    }
                 }
             }
+        } catch (WakeupException e) {
+            LOGGER.info("KafkaConsumer was waked up and going to be closed...");
+        } catch (Exception e) {
+            LOGGER.error("Unexpected error occurred", e);
+        } finally {
+            consumer.close();
+            LOGGER.info("KafkaConsumer was closed");
+            sync.countDown();
         }
+    }
+
+    public void stop() {
+        consumer.wakeup();
+        try {
+            sync.await();
+        } catch (InterruptedException e) {
+            // nothing
+        }
+    }
+
+    public static void main(String[] args) {
+        var consumerApp = new ConsumerApp();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            LOGGER.info("Shutdown was requested. Stopping ConsumerApp...");
+            consumerApp.stop();
+            LOGGER.info("ConsumerApp was gracefully stopped");
+        }));
+
+        consumerApp.start();
     }
 }
